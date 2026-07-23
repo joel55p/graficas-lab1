1954c0811f8dffddbbd75a566ebf72789f8c44a3
joel55p <joelnerio42@gmail.com>
2026-07-23 16:11:53 -0600

    Poligono 1 que es  amarillo con orilla blanca

--- src/main.zig
+++ src/main.zig
@@ -2,24 +2,45 @@ const std = @import("std");
 const rl = @cImport({
     @cInclude("raylib.h");
 });
+const fb = @import("framebuffer.zig");
+const Point = fb.Point;
+
+// Poligono 1: amarillo, orilla blanca
+const polygon1 = [_]Point{
+    .{ .x = 165, .y = 380 },
+    .{ .x = 185, .y = 360 },
+    .{ .x = 180, .y = 330 },
+    .{ .x = 207, .y = 345 },
+    .{ .x = 233, .y = 330 },
+    .{ .x = 230, .y = 360 },
+    .{ .x = 250, .y = 380 },
+    .{ .x = 220, .y = 385 },
+    .{ .x = 205, .y = 410 },
+    .{ .x = 193, .y = 383 },
+};
 
 pub fn main() !void {
-    const screenWidth = 800;
-    const screenHeight = 450;
+    const width = 800;
+    const height = 600;
+    var framebuffer = fb.Framebuffer.init(width, height, rl.BLACK);
 
-    rl.InitWindow(screenWidth, screenHeight, "Raylib + Zig");
-
+    rl.InitWindow(width, height, "Software Renderer - Poligono 1");
     defer rl.CloseWindow();
-
     rl.SetTargetFPS(60);
 
+    var exported = false;
+
     while (!rl.WindowShouldClose()) {
-        rl.BeginDrawing();
+        framebuffer.clear();
 
-        rl.ClearBackground(rl.RAYWHITE);
+        framebuffer.fill_polygon(&polygon1, rl.YELLOW);
+        framebuffer.draw_polygon_outline(&polygon1, rl.WHITE);
 
-        rl.DrawText("Funcional!", 250, 200, 30, rl.BLACK);
+        if (!exported) {
+            framebuffer.export_image("out.bmp");
+            exported = true;
+        }
 
-        rl.EndDrawing();
+        framebuffer.swap();
     }
 }
